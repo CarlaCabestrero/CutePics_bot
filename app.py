50b0ebb3edaf3934fc3ccd3241f3ec9b3ebcd9e3
CarlaCabestrero <carlacabes94@gmail.com>
2018-02-27 13:20:00 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import os, sys
+from flask import Flask, request
+from utils import wit_response, get_news_elements
+from pymessenger import Bot
+
+app = Flask(__name__)
+
+
+
+PAGE_ACCESS_TOKEN = "EAAEGgHE2HqsBACcxBhVaejIKhe9ovXabfs6842IuAgz5w5lgjZAdOJNL2zB5lOySFDszCZAHOQfULiyvjXrg8S8MeOWZAJuMWpYz1QcRKmZA6L5arp7i7MysEdKzu8vWQsCLQs8UYsN62jtZCTYs8tavZBjdRB313jFFVGAlliQQZDZD"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
+
+
+
+@app.route('/', methods =['GET'])
+def verify():
+
+    if request.args.get("hub.mode") == "subscribe" and            request.args.get("hub.challenge"):
+        if not request.args.get("hub.challenge"):
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
+    
+    
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    log(data)
+
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+                
+                #IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+                
+                #Check message type
+                if messaging_event.get('message'):
+                    if 'text' in messaging_event['message']:
+                        messaging_text  = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                categories = wit_response(messaging_text)
+
+                elements = get_news_elements(categories)
+                bot.send_generic_message(sender_id, elements)
+                    # #Echo
+                    # response = None
+
+                    # entity, value = wit_response(messaging_text)
+
+                    # if entity == 'newstype':
+                    # 	response = "Ok I will send you {} news".format(str(value))
+                    # elif entity == "location":
+                    # 	response = "Ok. So, you live in {0}. I will send you top headlines from {0}".format(str(value))
+                    
+                    # if response == None :
+                    #     response = "Sorry I didn't understand!"
+
+                    # bot.send_text_message(sender_id, response) 
+
+
+    return "ok", 200
+    
+def log(message):
+    print(message)
+    sys.stdout.flush()
+    
+if __name__ == "__main__":
+    app.run(debug= True, port = 80)
---
+++ utils.py
@@ -0,0 +1,49 @@
+from wit import Wit
+from gnewsclient import gnewsclient
+
+access_token = "6WHOSJPFPMFLSL4PQWVPL5RHWATOGZ7Q"
+
+client = Wit(access_token = access_token)
+
+def wit_response(message_text):
+	resp = client.message(message_text)
+	categories = {'newstype':None, 'location':None}
+
+	entities=list(resp['entities'])
+
+	for entity in entities:
+		categories[entity] = resp['entities'][entity][0]['value']
+
+	return categories
+
+def get_news_elements(categories):
+	news_client = gnewsclient()
+	news_client.query = ''
+
+	if categories['location'] != None:
+		news_client.query += categories['location']
+
+
+	if categories['newstype'] != None:
+		news_client.query += categories['newstype'] + ' '
+
+
+	news_items = news_client.get_news()
+	elements = []
+
+
+	for item in news_items:
+		element = {
+					'title': item['title'],
+					'buttons': [{
+								'type': 'web_url',
+								'title': "Read more",
+								'url': item['link']
+					}],
+					'image_url': item['img']
+		}
+		elements.append(element)
+
+	return elements
+
+#print(get_news_elements(wit_response("I want sports news in India")))
